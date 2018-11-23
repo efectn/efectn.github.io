050ec6ab4d71c2dc0ea439d914edccf8fb443497
M. Efe Çetin <45270788+efectn@users.noreply.github.com>
2018-11-23 17:39:40 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    alert("For a good navigation, please put the page in full screen with the button 'change view'");
+    alert("Developed By ♥ EfeCTN");
 });
